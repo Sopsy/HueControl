a65d33d00717c280d42521d760f8c740de8e8b7b
Sopsy <721951+Sopsy@users.noreply.github.com>
2021-01-15 23:02:01 +0100

    Update usageHelp

--- Hue/Bootstrap.php
+++ Hue/Bootstrap.php
@@ -56,29 +56,25 @@ final class Bootstrap
         return <<<EOL
         Usage: php cli.php <command> [args]
         
-        Available commands                                                              
-          CreateConfig [bridge_ip]   Detect the Hue Bridge, create an user and store
-                                       into config.json.
-                                       - bridge_ip: Force Hue Bridge IP, useful when
-                                         autodetection fails or when there are multiple
-                                         Hue Bridges in the same network.
-          DeleteResourceLinks <id>   Delete resource links by id
-          DeleteScene <id>           Delete a scene by id
-          DeleteUnusedMemorySensors  Delete memory sensors (boolean and integer flags)
-                                       that are not used by any rules
-          GetGroups                  List all groups (rooms)
-          GetLights                  List all lights
-          GetResourceLinks           List all resource links
-          GetRules                   List all rules
-          GetScenes [group_name]     List all scenes (all groups or a single group)
-                                       - group_name: List scenes only in this group
-          GetSensors                 List sensors (buttons, motion sensors, flags, etc.)
-          GetTemp                    List temperature sensors and their values
-          ProgramSensor [sensor_id] [group_id] [program_id]
-                                     Configure a sensor with a program
-                                       - sensor_id: Sensor to configure
-                                       - group_id: Group (room) to control
-                                       - program_id: Program to configure
+        Available commands:
+        CreateConfig [bridge_ip]   Detect the Hue Bridge, create an user and store into config.json.
+                                     - bridge_ip: Force Hue Bridge IP, useful when autodetection fails.
+        DeleteResourceLinks <id>   Delete resource links by id
+        DeleteScene <id>           Delete a scene by id
+        DeleteUnusedMemorySensors  Delete memory sensors (boolean and integer flags) that are not used by any rules
+        GetGroups                  List all groups (rooms)
+        GetLights                  List all lights
+        GetResourceLinks           List all resource links
+        GetRules                   List all rules
+        GetScenes [group_name]     List all scenes (all groups or a single group)
+                                     - group_name: List scenes only in this group
+        GetSensors                 List sensors (buttons, motion sensors, flags, etc.)
+        GetTemp                    List temperature sensors and their values
+        ProgramSensor [sensor_id] [group_id] [program_id]
+                                   Configure a sensor with a program
+                                     - sensor_id: Sensor to configure
+                                     - group_id: Group (room) to control
+                                     - program_id: Program to configure
         EOL;
     }
 }
